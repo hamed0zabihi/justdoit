377d10e8cf987432ea575b4d9bbf0880a3673241
hamed zabihi <hamed0zabihi@gmail.com>
2021-02-25 16:04:31 +0330

    for check - git diff

--- src/component/person.jsx
+++ src/component/person.jsx
@@ -21,7 +21,7 @@ import {Button} from "reactstrap"
 //         );
 //     }
 
-// } /*end class brackets */
+// } /*end class brackets y*/
 const Person=({name,family,age})=>{
     return(
         <p>
